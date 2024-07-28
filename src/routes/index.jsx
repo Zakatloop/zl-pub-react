7920cec66ade77d45e39dd244286a7734c630df6
fauzandwip <fauzan27dp@gmail.com>
2024-07-28 20:21:21 +0700

    feat: slicing/redesign footer for mobile and desktop

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,16 +1,19 @@
-import {
-    createBrowserRouter
-} from "react-router-dom";
+import { createBrowserRouter } from 'react-router-dom';
 // import Home from "../components/pages/Home";
-import OurValues from "../components/organism/OurValues";
+import OurValues from '../components/organism/OurValues';
+import Footer from '../components/organism/Footer';
 
 export const router = createBrowserRouter([
-    // {
-    //     path: "/",
-    //     element: <Home/>,
-    // },
-    {
-        path: "our-values",
-        element: <OurValues/>,
-    },
+  // {
+  //     path: "/",
+  //     element: <Home/>,
+  // },
+  {
+    path: 'our-values',
+    element: <OurValues />,
+  },
+  {
+    path: 'footer',
+    element: <Footer />,
+  },
 ]);
